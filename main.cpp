4156408781cff56f66484d2cad1c24d83203c3fd
filitov@gmail.com <filitov@gmail.com>
2023-01-12 16:49:44 +0800

    優先性整理在一起

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+int precendence(char op)
+{
+    if (op=='*' || op=='/')
+        return 2;
+    if (op=='+' || op=='-')
+        return 1;
+    return 0;
+}
+
+bool higher_or_equal(char op1, char op2)
+{
+    return precendence(op1) >= precendence(op2);
+}
+
 int main()
 {
     // char eq[501];
@@ -17,22 +31,16 @@ int main()
         {
         case '+':
         case '-':
-            while (!op.empty() ){
-                cout << "算符1: " << op.top() << endl;
-                op.pop();
-            }
-            op.push(*p);
-            break;
         case '*':
         case '/':
-            while (!op.empty()  && (op.top()!='+' && op.top()!='-')){
-                cout << "算符2: " << op.top() << endl;
+            while (!op.empty() && higher_or_equal(op.top(),*p) ) {
+                cout << "算符: " << op.top() << endl;
                 op.pop();
             }
             op.push(*p);
             break;
         default:
-            cout << "數字:  " << *p << endl;
+            cout << "數字: " << *p << endl;
         }
     }
 
